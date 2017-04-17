96bc08c75a04c0dbed7092669cf4f3ecaaa2ecd4
andyWynyard <andrew.wynyard@gmail.com>
2017-04-17 17:39:34 +0200

    final commit

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,7 +1,46 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
 
 public class CaesarCipher {
-	public static void main(String[] args) {
-		
+	/**
+	*Returns the carachter shifted by the given number of letters.
+	*/
+	public static char shiftLetter(char c, int n) {
+		int u = c;
+
+		if (! Character.isLetter(c))
+			return c;
+
+		u = u + n;
+		if (Character.isUpperCase(c) && u > 'Z' 
+		|| Character.isLowerCase(c) && u > 'z') {
+			u -= 26;
+		}
+		if (Character.isUpperCase(c) && u < 'A'
+		|| Character.isLowerCase(c) && u < 'a') {
+			u += 26;
+		}
+
+		return (char)u;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Scanner keyboard = new Scanner(System.in);
+		String plaintext, cipher = "", saveFileName = "cipher.txt";
+		int shift;
+		File f = new File(saveFileName);
+
+		Scanner fileIn = new Scanner(new File("text.txt"));
+		plaintext = fileIn.nextLine();
+		System.out.print("Shift (0-26): ");
+		shift = keyboard.nextInt();
+
+		for (int i = 0 ; i < plaintext.length() ; i++) {
+			cipher += shiftLetter( plaintext.charAt(i), shift);
+		}
+		PrintWriter out = new PrintWriter(f);
+			out.println(cipher);
+		out.close();
 	}
 }
